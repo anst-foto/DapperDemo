6b152dc0ff857eb1db27b368fba26e6d2259b7a6
Starinin Andrey <starininandrey@gmail.com>
2023-09-03 12:59:46 +0300

    Добавили класс DataBase

---
+++ DapperDemo.DataAccess/DataBase.cs
@@ -0,0 +1,8 @@
+﻿namespace DapperDemo.DataAccess;
+
+public class DataBase
+{
+    public TableRoles Roles { get; set; }
+    public TableAccounts Accounts { get; set; }
+    public TableUsers Users { get; set; }
+}
