f43b0ef90a1a246d2feb8d1948e149cf13f0f915
Starinin Andrey <starininandrey@gmail.com>
2023-09-03 11:46:17 +0300

    Добавление классов: Role, Account, User

---
+++ DapperDemo.Model/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace DapperDemo.Model;
+
+public class Account
+{
+    public int Id { get; set; }
+    public string Login { get; set; }
+    public string Password { get; set; }
+    public int RoleId { get; set; }
+    public bool IsActive { get; set; }
+}
