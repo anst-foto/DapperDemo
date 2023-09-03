9fc3ffe9b296dc2b842569099ce8fc0cc23ce227
Starinin Andrey <starininandrey@gmail.com>
2023-09-03 14:05:36 +0300

    Добавили инициализацию свойств

--- DapperDemo.DataAccess/DataBase.cs
+++ DapperDemo.DataAccess/DataBase.cs
@@ -5,4 +5,11 @@ public class DataBase
     public TableRoles Roles { get; set; }
     public TableAccounts Accounts { get; set; }
     public TableUsers Users { get; set; }
+
+    public DataBase(string connectionString)
+    {
+        Roles = new TableRoles(connectionString);
+        Accounts = new TableAccounts(connectionString);
+        Users = new TableUsers(connectionString);
+    }
 }
