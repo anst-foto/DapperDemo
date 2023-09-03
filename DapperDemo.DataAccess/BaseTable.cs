51e90d319568e3a32b7f0aa7adf312d0b9d9234c
Starinin Andrey <starininandrey@gmail.com>
2023-09-03 12:26:06 +0300

    Добавление библиотеки для доступа к БД

---
+++ DapperDemo.DataAccess/BaseTable.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+
+namespace DapperDemo.DataAccess;
+
+public class BaseTable<T>
+{
+    protected readonly SqliteConnection Db;
+
+    public BaseTable(string connectionString)
+    {
+        Db = new SqliteConnection(connectionString);
+    }
+
+    protected IEnumerable<T>? ExecuteWithResult(string sql)
+    {
+        Db.Open();
+
+        var result = Db.Query<T>(sql);
+        
+        Db.Close();
+
+        return result;
+    }
+
+    protected bool ExecuteWithNotResult(string sql)
+    {
+        Db.Open();
+
+        var result = Db.Execute(sql);
+        
+        Db.Close();
+
+        return result != 0;
+    }
+}
---
+++ DapperDemo.DataAccess/ICrud.cs
@@ -0,0 +1,10 @@
+﻿namespace DapperDemo.DataAccess;
+
+public interface ICrud<T>
+{
+    public bool Insert(T data);
+    public bool Update(T data);
+    public bool Delete(T data);
+    public IEnumerable<T>? FindAll();
+    public T? FindById(int id);
+}
